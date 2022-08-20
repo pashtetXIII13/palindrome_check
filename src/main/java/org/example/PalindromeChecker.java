51bfea69dbe007e62e6f9c557099ac06e76a083a
pashtet <pashtet.xiii@gmail.com>
2022-08-20 16:35:18 +0700

    Initial commit

---
+++ src/main/java/org/example/PalindromeChecker.java
@@ -0,0 +1,7 @@
+package org.example;
+
+/**
+ * @author pashtet
+ */
+public class PalindromeChecher {
+}
---
+++ src/test/java/org/example/PalindromeCheckerTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import junit.framework.TestCase;
+/**
+ * @author pashtet
+ */public class PalindromeCheckerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testCheck() {
+    }
+}
