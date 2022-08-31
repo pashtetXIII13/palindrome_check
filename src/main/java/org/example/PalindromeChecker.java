54aee8294f626b5f45e7d39728e8e5d49584a050
pashtet <pashtet.xiii@gmail.com>
2022-08-31 18:50:57 +0700

    add recursion method

--- src/main/java/org/example/PalindromeChecker.java
+++ src/main/java/org/example/PalindromeChecker.java
@@ -3,5 +3,23 @@ package org.example;
 /**
  * @author pashtet
  */
-public class PalindromeChecher {
+public class PalindromeChecker {
+
+    public boolean checkCycle(String inputString) {
+        if (inputString.isEmpty()) return true;
+        for (int i = 0; i < inputString.length() / 2; i++) {
+            if (inputString.charAt(i) != inputString.charAt(inputString.length() - i - 1)) return false;
+        }
+        return true;
+    }
+
+    public boolean checkRecursion(String inputString) {
+        if (inputString.isEmpty()) {
+            return true;
+        } else {
+            return inputString.length() == 1 ||
+                    (inputString.charAt(0) == inputString.charAt(inputString.length() - 1) &&
+                            checkRecursion(inputString.substring(1, inputString.length() - 1)));
+        }
+    }
 }
--- src/test/java/org/example/PalindromeCheckerTest.java
+++ src/test/java/org/example/PalindromeCheckerTest.java
@@ -1,14 +1,49 @@
 package org.example;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author pashtet
- */public class PalindromeCheckerTest extends TestCase {
+ */
+public class PalindromeCheckerTest {
+    PalindromeChecker palindromeChecker;
 
-    public void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void init() {
+        palindromeChecker = new PalindromeChecker();
     }
 
-    public void testCheck() {
+    @Test
+    public void testWithEmptyString() {
+        assertTrue(palindromeChecker.checkCycle(""));
+        assertTrue(palindromeChecker.checkRecursion(""));
+    }
+
+    @Test
+    public void testWithGoodPalindromeCase1() {
+        assertTrue(palindromeChecker.checkCycle("abcba"));
+        assertTrue(palindromeChecker.checkRecursion("abcba"));
+    }
+
+    @Test
+    public void testWithGoodPalindromeCase2() {
+        assertTrue(palindromeChecker.checkCycle("123321"));
+        assertTrue(palindromeChecker.checkRecursion("123321"));
+    }
+
+    @Test
+    public void testWithBadPalindromeCase1() {
+        assertFalse(palindromeChecker.checkCycle("123421"));
+        assertFalse(palindromeChecker.checkRecursion("123421"));
+    }
+
+    @Test
+    public void testWithBadPalindromeCase2() {
+        assertFalse(palindromeChecker.checkCycle("321321"));
+        assertFalse(palindromeChecker.checkRecursion("321321"));
     }
 }
